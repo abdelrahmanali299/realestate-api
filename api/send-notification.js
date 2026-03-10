d11c7c03ba9a33261b8ffd3804844e05e4b8d406
Abdelrahman <bedoalii255@gmail.com>
2026-03-11 00:23:17 +0200

    Fix export default function for Vercel API

--- api/send-notification.js
+++ api/send-notification.js
@@ -150,6 +150,7 @@ if (!admin.apps.length) {
             token_uri: process.env.FIREBASE_TOKEN_URI,
             auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
             client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+            universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
         }),
     });
 }
